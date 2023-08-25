6df9093e9a21dfb06f3d48c073b0224341f17716
Kushal1605 <kushalgupta1605@gmail.com>
2023-08-26 02:00:41 +0530

    EventListner Added

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -1,19 +1,22 @@
-function diceRoll() {
-    return Math.floor(Math.random()*6) + 1
-}
-var player1Score = diceRoll();
-var player2Score = diceRoll();
+document.querySelector("button").addEventListener("click", clickEvent);
 
-var player1Image = './images/dice' + player1Score + '.png';
-var player2Image = './images/dice' + player2Score + '.png';
 
-document.getElementById('img1').src = player1Image
-document.getElementById('img2').src = player2Image
+function clickEvent() {
 
-if(player1Score > player2Score) {
+    var player1Score = Math.floor(Math.random() * 6 + 1);
+    var player2Score = Math.floor(Math.random() * 6 + 1);
+
+    var player1Image = './images/dice' + player1Score + '.png';
+    var player2Image = './images/dice' + player2Score + '.png';
+
+    document.getElementById('img1').src = player1Image
+    document.getElementById('img2').src = player2Image
+
+    if(player1Score > player2Score) {
     document.getElementById('title').textContent = 'Player 1 Wins!'
-} else if(player1Score < player2Score) {
+    } else if(player1Score < player2Score) {
     document.getElementById('title').textContent = 'Player 2 Wins!'
-} else {
+    } else {
     document.getElementById('title').textContent = 'Draw!'
+    }
 }
