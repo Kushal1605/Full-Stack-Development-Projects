e16c2e9ddaec334824d4818cd3e08ca1a1925c74
Kushal1605 <kushalgupta1605@gmail.com>
2023-08-27 14:57:38 +0530

    update index.js

--- SimonGame/index.js
+++ SimonGame/index.js
@@ -1,112 +1,73 @@
-var gameOver = 0;
+var start = false;
+var color = ['green', 'red', 'yellow', 'blue']
 
-$(document).keydown(function(e) {
-    if((e.key === 'a' || e.key == 'A') && gameOver === 0) gameStart(e.key)
+var level = 0
+var userSequence = new String()
+var generatedSequence = new String()
+
+// Start the Game
+$('.game').click(function () {
+    if(!start) generateSequence()
+    start = true
 })
-function gameStart(key) {
 
-    gameOver = 1
-    var count = 0, userSequence = new String(), sequence = new String(), level = 1
-
-    // Start from Level 1
-    $('#header').text('Level: ' + level)  
-
-    // Initial blink  
-    sequence = generateRandNum(new String())   
-
-    // Counter to check for the sequence
-    count = 0
-
-    // User clicks on the coloured block
-    $('.item').off('click').on('click', function () {
-        console.log('curr Sequence: ' + sequence)
-        // Blink
+// Handling user click on the color blocks
+$('.item').off('click').on('click', function () {
+    // If game has started (If Game Over then it should not work)
+    if(start) {
+        userSequence += this.classList[2]
+        // User click Animation with sound
         $('.' + this.classList[2]).addClass('pressed')
-        
-        // Audio
         new Audio('./sounds/' + this.classList[1] + '.mp3').play()
-
-        // Delay
         setTimeout(() => {
-            $('.' + this.classList[2]).removeClass('pressed')
-        }, 500);
+        $('.' + this.classList[2]).removeClass('pressed')
+        }, 100);
 
-        // Storing user sequence
-        userSequence += this.classList[2].toString()
-        
-        // Wrong Sequence => Game Over
-        if(userSequence[count] !== sequence[count]) {
+        validateSequence(userSequence.length - 1)
+    }
+})
 
-            console.log('Game Over: ' + sequence + " " + userSequence)
-            $('#header').text('Game Over! Press A key to restart the game')
-
-            // Background Change
-            $('body').addClass('gameOver')
-
-            // Game Over Audio
-            new Audio('./sounds/wrong.mp3').play()
-
-            // Delay
-            setTimeout(() => {
-                $('body').removeClass('gameOver')
-            }, 500);
-
-            // KeyPress event to work again
-            gameOver = 0
-            
-            // Reset sequence and user sequence
-            sequence = new String()
-            userSequence = new String()
-            return
+// Validate if sequence is correct or not
+function validateSequence(index) {
+    // If correct
+    if(userSequence[index] === generatedSequence[index]) {
+        // If level completed then move to next level and generate new sequence
+        if(userSequence.length == generatedSequence.length) {
+        setTimeout(() => {
+            generateSequence()
+        }, 1000);
         }
-        count += 1 
-        if(userSequence.length == sequence.length)  {
-
-            // Delay for next blink
-            setTimeout(() => {
-                sequence = generateRandNum(sequence)
-            }, 1000);
-
-            // Resetting the user sequence and counter before moving to the next level
-            count = 0
-            userSequence = new String()
-
-            // Moving to next level
-            level += 1;
-
-            // Update Level
-            $('#header').text('Level: ' + level)
-        }
-    })
+    }
+    // Incorrect Sequence => Game Over! 
+    else {
+        $('#header').text('Game Over!')
+        $('body').addClass('gameOver')
+        new Audio('./sounds/wrong.mp3').play();
+        setTimeout(() => {
+            $('body').removeClass('gameOver')
+        }, 200);
+        $('.game').text('Start Again')
+        startOver()
+    }
 }
 
-function generateRandNum(sequence) {
-    // Random blink
-    var randNum = Math.floor(Math.random() * 4)
-    $('.' + randNum).addClass('pressed')
-    switch (randNum) {
-        case 0:
-            new Audio('./sounds/green.mp3').play()
-            break;
-        case 1:
-            new Audio('./sounds/red.mp3').play()
-            break;
-        case 2:
-            new Audio('./sounds/yellow.mp3').play()
-            break;
-        case 3:
-            new Audio('./sounds/blue.mp3').play()
-            break;        
-        default:
-            break;
-    }
-    setTimeout(() => {
-        $('.' + randNum).removeClass('pressed')
-    }, 500);
-
-    // Storing the random sequence to match with the user sequence
-    sequence += randNum.toString()
-    console.log(sequence)
-
-    return sequence
+// If Game Over then start from level 1
+function startOver() {
+    level = 0
+    userSequence = new String()
+    generatedSequence = new String()
+    start = false
+}
+// A random blink to generate Sequence for user
+function generateSequence() {
+    userSequence = new String()
+    // Update Level
+    level += 1
+    $('#header').text('Level ' + level)
+    
+    // Random color block to blink with Audio
+    var num = Math.floor(Math.random() * 4)
+    new Audio('./sounds/' + color[num] + '.mp3').play()
+    $('.' + color[num]).fadeIn(100).fadeOut(100).fadeIn(100)
+    generatedSequence += num.toString()
 }
